c85257af088c45d6086f44943909b4ff730ba18c
Ky Lee <kyulee@live.com>
2018-06-11 21:31:58 -0700

    Linked List Components

---
+++ LinkedListComponents/t1.cs
@@ -0,0 +1,63 @@
+/*
+We are given head, the head node of a linked list containing unique integer values.
+
+We are also given the list G, a subset of the values in the linked list.
+
+Return the number of connected components in G, where two values are connected if they appear consecutively in the linked list.
+
+Example 1:
+
+Input: 
+head: 0->1->2->3
+G = [0, 1, 3]
+Output: 2
+Explanation: 
+0 and 1 are connected, so [0, 1] and [3] are the two connected components.
+Example 2:
+
+Input: 
+head: 0->1->2->3->4
+G = [0, 3, 1, 4]
+Output: 2
+Explanation: 
+0 and 1 are connected, 3 and 4 are connected, so [0, 1] and [3, 4] are the two connected components.
+Note:
+
+If N is the length of the linked list given by head, 1 <= N <= 10000.
+The value of each node in the linked list will be in the range [0, N - 1].
+1 <= G.length <= 10000.
+G is a subset of all values in the linked list.
+
+*/
+// Comment: Use set. Not so interesting
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int NumComponents(ListNode head, int[] G) {
+        HashSet<int> s = new HashSet<int>();
+        foreach(var g in G) s.Add(g);
+        
+        ListNode curr = head;
+        int cnt = 0;
+  
+        while(curr != null) {
+            int val = curr.val;
+            if (s.Contains(val)) {
+                cnt++;
+                while(curr != null && s.Contains(curr.val)) {
+                    curr = curr.next;
+                }
+                continue;
+            }
+            curr = curr.next;
+        }
+        return cnt;
+    }
+}
+
