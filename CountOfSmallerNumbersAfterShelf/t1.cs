8fbada63d3297776b9e0cfa533509df74566aa3c
Ky Lee <kyulee@live.com>
2018-10-10 15:39:53 -0700

    Another one for Count of Smaller Numbers After Self

--- CountOfSmallerNumbersAfterShelf/t1.cs
+++ CountOfSmallerNumbersAfterShelf/t1.cs
@@ -18,6 +18,68 @@ To the right of 1 there is 0 smaller element.
 // When new node traverse to the left child (smaller than the existing node), then book-keep such smaller count to the root/current node.
 // When new ndoe traverse to the right child, update/add the root's left child count to the cnt of the current node which would be the answer -- note this won't change while the left child count might change. Two properties are the key.
 
+// Another one first. Node contains small or equal childs
+public class Solution
+{
+    class Node
+    {
+        public Node(int v) { val = v; lcnt = 1; cnt = 1; }
+        public int val;
+        public int lcnt; // small or equal childs
+        public int cnt; // Spoiler:: Track equal count for duplication
+        public Node left;
+        public Node right;
+    }
+    int Rec(Node r, int val, int curr)
+    {
+        if (r.val == val) {
+            // Spoiler::
+            var t = r.lcnt - r.cnt; // # of strictly smaller than curr
+            r.cnt++;
+            r.lcnt++;
+            return curr + t;
+        }
+        else if (r.val > val)
+        {
+            r.lcnt++;
+            if (r.left == null)
+            {
+                r.left = new Node(val);
+                return curr;
+            }
+            return Rec(r.left, val, curr);
+        }
+        else
+        {
+            curr += r.lcnt;
+            if (r.right == null)
+            {
+                r.right = new Node(val);
+                return curr;
+            }
+            return Rec(r.right, val, curr);
+        }
+    }
+    
+    public IList<int> CountSmaller(int[] nums)
+    {
+        List<int> ans = new List<int>();
+        int len = nums.Length;
+        if (len == 0) return ans;
+        Node r = new Node(nums[len-1]);
+        ans.Add(0);
+
+        // main loop
+        for(int i=len-2; i>=0; i--)
+        {
+            ans.Add(
+            Rec(r, nums[i],0));
+        }
+
+        ans.Reverse();
+        return ans;
+    }
+}
 public class Solution
 {
     class Node
